329e4aa3b871e93a5fe3e2ab8234597b33e92f6e
Zuhaib Ulla shariff <zuhaibullashariff7862@gmail.com>
2026-09-10 11:03:07 +0530

    fix: add type annotation to matchFallback to fix TS build error

--- src/app/image-sitemap.xml/route.ts
+++ src/app/image-sitemap.xml/route.ts
@@ -46,7 +46,7 @@ export async function GET() {
       
       // Fallback if alt is not defined but src is
       const fallbackRegex = /<img[^>]+src="([^">]+)"/g;
-      let matchFallback;
+      let matchFallback: RegExpExecArray | null;
       while ((matchFallback = fallbackRegex.exec(blog.content)) !== null) {
         if (matchFallback[1] && !images.some(img => img.loc === matchFallback[1])) {
           images.push({
